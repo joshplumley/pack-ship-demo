78227958a3298959b811782a8570217e5b192343
Josh Plumley <plumleyj@gmail.com>
2022-01-04 22:11:48 -0500

    url from .env works now

--- frontend/src/services/server.js
+++ frontend/src/services/server.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-let { API_URL } = process.env;
+let { REACT_APP_API_URL } = process.env;
 
 export const API = {
     async getPackingQueue() {
         try {
-            const response = await axios.get(`${API_URL}/workOrders/packingQueue`,);
+            const response = await axios.get(`${REACT_APP_API_URL}/workOrders/packingQueue`,);
             return response.data;
         } catch (error) {
             console.error("getPackingQueue", error);
