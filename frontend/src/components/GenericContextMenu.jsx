0555c552f2832d81ef2b2facd852f7e7af6b6230
Zachary Hull <zacharyhull@Hermes.local>
2022-01-19 21:34:09 -0500

    added generic context menu

---
+++ frontend/src/components/GenericContextMenu.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Menu from '@mui/material/Menu';
+
+export default function ContextMenu({children, menuPosition, setMenuPosition}) {
+  const open = Boolean(menuPosition)
+
+  const handleClose = (event) => {
+    setMenuPosition(null);
+  };
+
+  return (
+      <div>
+        <Menu
+          open={open}
+          anchorReference="anchorPosition"
+          anchorPosition={menuPosition}
+          anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+          }}
+          onClose={handleClose}
+          role={undefined}
+          placement="bottom-start"
+          transition
+          disablePortal
+        >
+          {children}
+        </Menu>
+      </div>
+  );
+}
